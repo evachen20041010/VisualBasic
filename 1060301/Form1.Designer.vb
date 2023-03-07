86edb7eaeaf065ceaeb01ef618184de6b3d5f4f0
CSIE <CSIE@DESKTOP-1K0PT5B>
2023-03-07 11:26:35 +0800

    加入專案檔案。

---
+++ 1060301/Form1.Designer.vb
@@ -0,0 +1,62 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 覆寫 Dispose 以清除元件清單。
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '為 Windows Form 設計工具的必要項
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下為 Windows Form 設計工具所需的程序
+    '可以使用 Windows Form 設計工具進行修改。
+    '請勿使用程式碼編輯器進行修改。
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.txt_1 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(456, 78)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(214, 95)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "第一題"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'txt_1
+        '
+        Me.txt_1.Location = New System.Drawing.Point(294, 265)
+        Me.txt_1.Multiline = True
+        Me.txt_1.Name = "txt_1"
+        Me.txt_1.Size = New System.Drawing.Size(580, 286)
+        Me.txt_1.TabIndex = 1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(13.0!, 24.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1269, 738)
+        Me.Controls.Add(Me.txt_1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents txt_1 As TextBox
+End Class
---
+++ 1060301/Form1.vb
@@ -0,0 +1,23 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim m0, m1, m2, m3, m4
+        FileOpen(1, "C:\Users\CSIE\source\repos\1060301\1060301.sm", OpenMode.Input)
+        Input(1, m0)
+        FileClose()
+        m1 = m0
+        For i = 1 To 9
+            m2 = m1 \ 10
+            m3 = m1 Mod 10
+            m4 = m4 & m3
+            If m2 = 0 Then Exit For
+            m1 = m2
+        Next
+
+        If m0 = m4 Then
+            txt_1.Text = "第一題結果：" & m0 & "is a plaindrome"
+        Else
+            txt_1.Text = "第一題結果：" & m0 & "is not a plaindrome"
+        End If
+
+    End Sub
+End Class
