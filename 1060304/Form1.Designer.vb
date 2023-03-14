fb214586c233f49550012bc9cc3b461e608dbf46
evachen20041010 <a0937510149@gmail.com>
2023-03-14 11:57:05 +0800

    加入專案檔案。

---
+++ 1060304/Form1.Designer.vb
@@ -0,0 +1,62 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 覆寫 Dispose 以清除元件清單。
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '為 Windows Form 設計工具的必要項
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下為 Windows Form 設計工具所需的程序
+    '可以使用 Windows Form 設計工具進行修改。
+    '請勿使用程式碼編輯器進行修改。
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.txt_4 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(456, 78)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(214, 95)
+        Me.Button4.TabIndex = 0
+        Me.Button4.Text = "第四題"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'txt_4
+        '
+        Me.txt_4.Location = New System.Drawing.Point(294, 265)
+        Me.txt_4.Multiline = True
+        Me.txt_4.Name = "txt_4"
+        Me.txt_4.Size = New System.Drawing.Size(580, 286)
+        Me.txt_4.TabIndex = 1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(13.0!, 24.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1269, 738)
+        Me.Controls.Add(Me.txt_4)
+        Me.Controls.Add(Me.Button4)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button4 As Button
+    Friend WithEvents txt_4 As TextBox
+End Class
---
+++ 1060304/Form1.vb
@@ -0,0 +1,23 @@
+﻿Public Class Form1
+    Private Sub btn_03_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Dim h(3), w(3), bmi(3), s_bmi
+        FileOpen(1, "C:\Users\CSIE\source\repos\1060304\1060304.sm", OpenMode.Input)
+        For i = 1 To 3
+            Input(1, h(i)) : Input(1, w(i))
+            bmi(i) = w(i) / (h(i) / 100) ^ 2
+        Next
+        FileClose(1)
+
+        s_bmi = bmi(1)
+        For i = 1 To 3
+            If bmi(i) < s_bmi Then s_bmi = bmi(i)
+        Next
+        Dim s_bmi45 As Integer = s_bmi
+
+        If s_bmi45 >= 20 And s_bmi45 <= 25 Then
+            txt_4.Text = "第四題結果:最小bmi值" & s_bmi45 & "，正常"
+        Else
+            txt_4.Text = "第四題結果:最大bmi值" & s_bmi45 & "，不正常"
+        End If
+    End Sub
+End Class
