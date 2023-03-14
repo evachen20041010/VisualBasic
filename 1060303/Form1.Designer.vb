070abfc7f772e1c65bebcb60b868a224656c3ff5
evachen20041010 <a0937510149@gmail.com>
2023-03-14 11:55:39 +0800

    加入專案檔案。

---
+++ 1060303/Form1.Designer.vb
@@ -0,0 +1,62 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 覆寫 Dispose 以清除元件清單。
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '為 Windows Form 設計工具的必要項
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下為 Windows Form 設計工具所需的程序
+    '可以使用 Windows Form 設計工具進行修改。
+    '請勿使用程式碼編輯器進行修改。
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.txt_3 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(456, 78)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(214, 95)
+        Me.Button3.TabIndex = 0
+        Me.Button3.Text = "第三題"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'txt_3
+        '
+        Me.txt_3.Location = New System.Drawing.Point(294, 265)
+        Me.txt_3.Multiline = True
+        Me.txt_3.Name = "txt_3"
+        Me.txt_3.Size = New System.Drawing.Size(580, 286)
+        Me.txt_3.TabIndex = 1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(13.0!, 24.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1269, 738)
+        Me.Controls.Add(Me.txt_3)
+        Me.Controls.Add(Me.Button3)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button3 As Button
+    Friend WithEvents txt_3 As TextBox
+End Class
---
+++ 1060303/Form1.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form1
+    Private Sub btn_03_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim m0, m1, m2, m3, m4
+        FileOpen(1, "C:\Users\CSIE\source\repos\1060303\1060303.sm", OpenMode.Input)
+        Input(1, m0)
+        FileClose(1)
+
+        For i = 1 To m0
+            If m0 Mod i = 0 Then m1 = m1 + 1
+        Next
+        If m1 = 2 Then
+            txt_3.Text = "第三題結果:" & m0 & "is a prime number."
+        Else
+            txt_3.Text = "第三題結果:" & m0 & "is not a prime number."
+        End If
+    End Sub
+End Class
