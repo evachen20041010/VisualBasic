de80fd13610e4a6c65b033538d89499d012faf87
evachen20041010 <a0937510149@gmail.com>
2023-03-21 11:26:59 +0800

    加入專案檔案。

---
+++ 1060305/UserControl1.Designer.vb
@@ -0,0 +1,61 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class UserControl1
+    Inherits System.Windows.Forms.UserControl
+
+    'UserControl1 覆寫 Dispose 以清除元件清單。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '為 Windows Form 設計工具的必要項
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下為 Windows Form 設計工具所需的程序
+    '可以使用 Windows Form 設計工具進行修改。
+    '請勿使用程式碼編輯器進行修改。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.txt_5 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(361, 101)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(258, 128)
+        Me.Button5.TabIndex = 0
+        Me.Button5.Text = "第五題"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'txt_5
+        '
+        Me.txt_5.Location = New System.Drawing.Point(189, 271)
+        Me.txt_5.Multiline = True
+        Me.txt_5.Name = "txt_5"
+        Me.txt_5.Size = New System.Drawing.Size(614, 339)
+        Me.txt_5.TabIndex = 1
+        '
+        'UserControl1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(13.0!, 24.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.txt_5)
+        Me.Controls.Add(Me.Button5)
+        Me.Name = "UserControl1"
+        Me.Size = New System.Drawing.Size(1041, 660)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button5 As Button
+    Friend WithEvents txt_5 As TextBox
+End Class
---
+++ 1060305/UserControl1.vb
@@ -0,0 +1,28 @@
+﻿Public Class UserControl1
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Dim a(2, 2), b(2, 2), c(2, 2)
+        FileOpen(1, "C:\Users\CSIE\source\repos\1060305\1060305\1060305.SM", OpenMode.Input)
+        Input(1, a(1, 1)) : Input(1, a(1, 2))
+        Input(1, a(2, 1)) : Input(1, a(2, 2))
+        Input(1, b(1, 1)) : Input(1, b(1, 2))
+        Input(1, b(2, 1)) : Input(1, b(2, 2))
+        FileClose()
+
+        Dim i = 1
+        Do
+            Dim j = 1
+            Do
+                c(i, j) = a(i, j) + b(i, j)
+                j = j + 1
+
+            Loop While j <= 2
+            i = i + 1
+        Loop While i <= 2
+
+        Dim m1
+        m1 = m1 & "[" & c(1, 1) & "       " & c(1, 2) & "]" & vbNewLine
+        m1 = m1 & "[" & c(2, 1) & "   " & c(2, 2) & "]" & vbNewLine
+
+        txt_5.Text = "第五題結果：" & vbNewLine & m1
+    End Sub
+End Class
