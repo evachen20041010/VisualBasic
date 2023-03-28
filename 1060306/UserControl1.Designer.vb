2fc3bcf76178d4db2845a2c23aa06eaf13af60d5
evachen20041010 <a0937510149@gmail.com>
2023-03-28 11:41:51 +0800

    加入專案檔案。

---
+++ 1060306/UserControl1.Designer.vb
@@ -0,0 +1,155 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class UserControl1
+    Inherits System.Windows.Forms.UserControl
+
+    'UserControl1 覆寫 Dispose 以清除元件清單。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '為 Windows Form 設計工具的必要項
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下為 Windows Form 設計工具所需的程序
+    '可以使用 Windows Form 設計工具進行修改。
+    '請勿使用程式碼編輯器進行修改。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.DGV = New System.Windows.Forms.DataGridView()
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.DGV, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.TextBox4)
+        Me.GroupBox1.Controls.Add(Me.TextBox3)
+        Me.GroupBox1.Controls.Add(Me.TextBox2)
+        Me.GroupBox1.Controls.Add(Me.TextBox1)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(70, 70)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(2103, 529)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "應檢人資料"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(299, 89)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(58, 24)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "姓名"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(1294, 89)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(130, 24)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "准考證號碼"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(299, 303)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(58, 24)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "座號"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(1318, 303)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(106, 24)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "考試日期"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(390, 77)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(223, 36)
+        Me.TextBox1.TabIndex = 1
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(1444, 77)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(223, 36)
+        Me.TextBox2.TabIndex = 4
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(390, 291)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(223, 36)
+        Me.TextBox3.TabIndex = 5
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(1444, 291)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(223, 36)
+        Me.TextBox4.TabIndex = 6
+        '
+        'DGV
+        '
+        Me.DGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DGV.Location = New System.Drawing.Point(70, 649)
+        Me.DGV.Name = "DGV"
+        Me.DGV.RowHeadersWidth = 82
+        Me.DGV.RowTemplate.Height = 38
+        Me.DGV.Size = New System.Drawing.Size(2103, 645)
+        Me.DGV.TabIndex = 1
+        '
+        'UserControl1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(13.0!, 24.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.DGV)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "UserControl1"
+        Me.Size = New System.Drawing.Size(2271, 1399)
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        CType(Me.DGV, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents DGV As DataGridView
+End Class
---
+++ 1060306/UserControl1.vb
@@ -0,0 +1,55 @@
+﻿Public Class UserControl1
+    Dim d(100, 4), rno, ec
+    Private Sub UserControl1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Call rdata()
+        For i = 1 To rno
+            ec = ""
+            If ec = "" Then Call sp1(i)
+            d(i, 4) = ec
+        Next
+        Call wdata()
+    End Sub
+
+    Sub rdata()
+        FileOpen(1, "C:\Users\CSIE\source\repos\1060306\1060306.SM", OpenMode.Input)
+        rno = 0
+        Do While Not EOF(1)
+            rno = rno + 1
+            For j = 1 To 3
+                Input(1, d(rno, j))
+            Next
+        Loop
+        FileClose(1)
+    End Sub
+
+    Sub sp1(ByVal i)
+        Dim idno = d(i, 1)
+        Dim m1 = Len(idno)
+        If m1 <> 10 Then ec = "format error"
+
+        Dim m2 = Mid(idno, 1, 1)
+        If m2 < "A" Or m2 > "Z" Then ec = "format error"
+
+        For j = 2 To 10
+            Dim m3 = Mid(idno, j, 1)
+            If m3 < "0" Or m3 > "9" Then ec = "format error"
+        Next
+    End Sub
+
+    Sub wdata()
+        Dim dt As New DataTable
+        dt.Columns.Add("ID_NO")
+        dt.Columns.Add("NAME")
+        dt.Columns.Add("SEX")
+        dt.Columns.Add("ERROR")
+        For i = 1 To rno
+            Dim dr As DataRow = dt.NewRow
+            dr(0) = d(i, 1)
+            dr(3) = d(i, 4)
+            dt.Rows.Add(dr)
+        Next
+        DGV.DataSource = dt
+
+    End Sub
+
+End Class
