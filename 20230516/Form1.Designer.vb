dee2242ee77f30c162fe12675f01b05eaa43c7c8
evachen20041010 <a0937510149@gmail.com>
2023-05-16 11:37:05 +0800

    加入專案檔案。

---
+++ 20230516/Form1.Designer.vb
@@ -0,0 +1,125 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 覆寫 Dispose 以清除元件清單。
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '為 Windows Form 設計工具的必要項
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下為 Windows Form 設計工具所需的程序
+    '可以使用 Windows Form 設計工具進行修改。
+    '請勿使用程式碼編輯器進行修改。
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.btn_1 = New System.Windows.Forms.Button()
+        Me.btn_2 = New System.Windows.Forms.Button()
+        Me.btn_3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.txt_1 = New System.Windows.Forms.TextBox()
+        Me.txt_2 = New System.Windows.Forms.TextBox()
+        Me.txt_3 = New System.Windows.Forms.TextBox()
+        Me.pd = New System.Drawing.Printing.PrintDocument()
+        Me.SuspendLayout()
+        '
+        'btn_1
+        '
+        Me.btn_1.Location = New System.Drawing.Point(160, 67)
+        Me.btn_1.Name = "btn_1"
+        Me.btn_1.Size = New System.Drawing.Size(168, 58)
+        Me.btn_1.TabIndex = 0
+        Me.btn_1.Text = "第一題"
+        Me.btn_1.UseVisualStyleBackColor = True
+        '
+        'btn_2
+        '
+        Me.btn_2.Location = New System.Drawing.Point(524, 67)
+        Me.btn_2.Name = "btn_2"
+        Me.btn_2.Size = New System.Drawing.Size(168, 58)
+        Me.btn_2.TabIndex = 1
+        Me.btn_2.Text = "第二題"
+        Me.btn_2.UseVisualStyleBackColor = True
+        '
+        'btn_3
+        '
+        Me.btn_3.Location = New System.Drawing.Point(936, 67)
+        Me.btn_3.Name = "btn_3"
+        Me.btn_3.Size = New System.Drawing.Size(168, 58)
+        Me.btn_3.TabIndex = 2
+        Me.btn_3.Text = "第三題"
+        Me.btn_3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(1291, 67)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(168, 58)
+        Me.Button4.TabIndex = 3
+        Me.Button4.Text = "列印答案"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'txt_1
+        '
+        Me.txt_1.Location = New System.Drawing.Point(58, 183)
+        Me.txt_1.Multiline = True
+        Me.txt_1.Name = "txt_1"
+        Me.txt_1.Size = New System.Drawing.Size(348, 555)
+        Me.txt_1.TabIndex = 4
+        '
+        'txt_2
+        '
+        Me.txt_2.Location = New System.Drawing.Point(432, 183)
+        Me.txt_2.Multiline = True
+        Me.txt_2.Name = "txt_2"
+        Me.txt_2.Size = New System.Drawing.Size(365, 555)
+        Me.txt_2.TabIndex = 5
+        '
+        'txt_3
+        '
+        Me.txt_3.Location = New System.Drawing.Point(830, 183)
+        Me.txt_3.Multiline = True
+        Me.txt_3.Name = "txt_3"
+        Me.txt_3.Size = New System.Drawing.Size(384, 555)
+        Me.txt_3.TabIndex = 6
+        '
+        'pd
+        '
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(13.0!, 24.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1533, 872)
+        Me.Controls.Add(Me.txt_3)
+        Me.Controls.Add(Me.txt_2)
+        Me.Controls.Add(Me.txt_1)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.btn_3)
+        Me.Controls.Add(Me.btn_2)
+        Me.Controls.Add(Me.btn_1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btn_1 As Button
+    Friend WithEvents btn_2 As Button
+    Friend WithEvents btn_3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents txt_1 As TextBox
+    Friend WithEvents txt_2 As TextBox
+    Friend WithEvents txt_3 As TextBox
+    Friend WithEvents pd As Printing.PrintDocument
+End Class
---
+++ 20230516/Form1.vb
@@ -0,0 +1,86 @@
+﻿Public Class Form1
+    Private Sub btn_1_Click(sender As Object, e As EventArgs) Handles btn_1.Click
+        Dim m0, m1, m2, m3, m4
+        FileOpen(1, "C:\Users\CSIE\source\repos\20230516\1060301.sm", OpenMode.Input)
+        Input(1, m0)
+        FileClose()
+        m1 = m0
+        For i = 1 To 9
+            m2 = m1 \ 10
+            m3 = m1 Mod 10
+            m4 = m4 & m3
+            If m2 = 0 Then Exit For
+            m1 = m2
+        Next
+
+        If m0 = m4 Then
+            txt_1.Text = "第一題結果：" & m0 & "is a plaindrome"
+        Else
+            txt_1.Text = "第一題結果：" & m0 & "is not a plaindrome"
+        End If
+    End Sub
+
+    Private Sub btn_2_Click(sender As Object, e As EventArgs) Handles btn_2.Click
+        Dim m0, m1, m2, m3, m4
+        FileOpen(1, "C:\Users\CSIE\source\repos\20230516\1060303.sm", OpenMode.Input)
+        Input(1, m0)
+        FileClose(1)
+
+        For i = 1 To m0
+            If m0 Mod i = 0 Then m1 = m1 + 1
+        Next
+        If m1 = 2 Then
+            txt_2.Text = "第三題結果:" & m0 & "is a prime number."
+        Else
+            txt_2.Text = "第三題結果:" & m0 & "is not a prime number."
+        End If
+    End Sub
+
+    Private Sub btn_3_Click(sender As Object, e As EventArgs) Handles btn_3.Click
+        Dim a(2, 2), b(2, 2), c(2, 2)
+        FileOpen(1, "C:\Users\CSIE\source\repos\20230516\1060305.SM", OpenMode.Input)
+        Input(1, a(1, 1)) : Input(1, a(1, 2))
+        Input(1, a(2, 1)) : Input(1, a(2, 2))
+        Input(1, b(1, 1)) : Input(1, b(1, 2))
+        Input(1, b(2, 1)) : Input(1, b(2, 2))
+        FileClose()
+
+        Dim i = 1
+        Do
+            Dim j = 1
+            Do
+                c(i, j) = a(i, j) + b(i, j)
+                j = j + 1
+
+            Loop While j <= 2
+            i = i + 1
+        Loop While i <= 2
+
+        Dim m1
+        m1 = m1 & "[" & c(1, 1) & "       " & c(1, 2) & "]" & vbNewLine
+        m1 = m1 & "[" & c(2, 1) & "   " & c(2, 2) & "]" & vbNewLine
+
+        txt_3.Text = "第五題結果：" & vbNewLine & m1
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Call pd.Print()
+    End Sub
+
+    Private Sub pd_PrintPage(sender As Object, e As Printing.PrintPageEventArgs) Handles pd.PrintPage
+        Dim m0
+        Dim vn = vbNewLine
+        m0 = m0 & "姓名：[陳紋誼] 准考證號碼：[999999]" & vn
+        m0 = m0 & "座號：[01] 日期：[2018/01/21]" & vn & vn
+        m0 = m0 & txt_1.Text & vn & vn
+        m0 = m0 & txt_2.Text & vn & vn
+        m0 = m0 & txt_3.Text & vn & vn
+
+        Dim mtxt = m0
+        Dim mfont = txt_1.Font
+        Dim mcolor = Brushes.Black
+        Dim x = 100
+        Dim y = 100
+        e.Graphics.DrawString(mtxt, mfont, mcolor, x, y)
+    End Sub
+End Class
