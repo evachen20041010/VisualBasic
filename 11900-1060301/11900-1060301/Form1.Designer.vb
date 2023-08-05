2887f8f56a56489c4f071359daa45588b07d8dd9
EvaChen <82190631+evachen20041010@users.noreply.github.com>
2023-08-05 19:52:18 +0800

    Add files via upload

---
+++ 11900-1060301/11900-1060301/Form1.Designer.vb
@@ -0,0 +1,79 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 覆寫 Dispose 以清除元件清單。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '為 Windows Form 設計工具的必要項
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下為 Windows Form 設計工具所需的程序
+    '可以使用 Windows Form 設計工具進行修改。
+    '請勿使用程式碼編輯器進行修改。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btn_01 = New System.Windows.Forms.Button()
+        Me.btn_print = New System.Windows.Forms.Button()
+        Me.txt_01 = New System.Windows.Forms.TextBox()
+        Me.pd = New System.Drawing.Printing.PrintDocument()
+        Me.SuspendLayout()
+        '
+        'btn_01
+        '
+        Me.btn_01.Location = New System.Drawing.Point(80, 43)
+        Me.btn_01.Name = "btn_01"
+        Me.btn_01.Size = New System.Drawing.Size(95, 29)
+        Me.btn_01.TabIndex = 0
+        Me.btn_01.Text = "第一題"
+        Me.btn_01.UseVisualStyleBackColor = True
+        '
+        'btn_print
+        '
+        Me.btn_print.Location = New System.Drawing.Point(248, 43)
+        Me.btn_print.Name = "btn_print"
+        Me.btn_print.Size = New System.Drawing.Size(95, 29)
+        Me.btn_print.TabIndex = 1
+        Me.btn_print.Text = "列印答案"
+        Me.btn_print.UseVisualStyleBackColor = True
+        '
+        'txt_01
+        '
+        Me.txt_01.Location = New System.Drawing.Point(80, 97)
+        Me.txt_01.Multiline = True
+        Me.txt_01.Name = "txt_01"
+        Me.txt_01.Size = New System.Drawing.Size(360, 240)
+        Me.txt_01.TabIndex = 2
+        '
+        'pd
+        '
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.txt_01)
+        Me.Controls.Add(Me.btn_print)
+        Me.Controls.Add(Me.btn_01)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btn_01 As Button
+    Friend WithEvents btn_print As Button
+    Friend WithEvents txt_01 As TextBox
+    Friend WithEvents pd As Printing.PrintDocument
+End Class
---
+++ 11900-1060301/11900-1060301/Form1.vb
@@ -0,0 +1,75 @@
+﻿Public Class Form1
+    '******************************
+    '* 11900-1060301 Program Start *
+    '******************************
+    Private Sub btn_01_Click(sender As Object, e As EventArgs) Handles btn_01.Click
+        Dim m0 As Integer   '存參考資料的數字
+
+        FileOpen(1, "C:\Users\mshome\source\11900\範例光碟\1060301.SM", OpenMode.Input) '開啟檔案(模式：讀取)
+        Input(1, m0)    '存檔案的值
+        FileClose(1)    '關檔
+
+        Dim i, m1, m2, m3 As Integer
+        Dim m4 As String = ""   '存倒過來的數字串
+        m1 = m0
+
+        '將數字串倒過來存入m0(For...Next)
+        'For i = 1 To 9
+        '    m2 = m1 Mod 10
+        '    m3 = m1 \ 10
+        '    m4 = m4 & m2
+        '    If m3 = 0 Then
+        '        Exit For
+        '    End If
+        '    m1 = m3
+        'Next
+
+        '將數字串倒過來存入m0(Do While...Loop)
+        'i = 0
+        'Do While i <= 9
+        '    m2 = m1 Mod 10
+        '    m3 = m1 \ 10
+        '    m4 = m4 & m2
+        '    If m3 = 0 Then
+        '        Exit Do
+        '    End If
+        '    m1 = m3
+        '    i = i + 1
+        'Loop
+
+        '將數字串倒過來存入m0(Do...Loop While)
+        i = 0
+        Do
+            m2 = m1 Mod 10
+            m3 = m1 \ 10
+            m4 = m4 & m2
+            If m3 = 0 Then
+                Exit Do
+            End If
+            m1 = m3
+            i = i + 1
+        Loop While i <= 9
+
+        '判斷數字串是不是迴文
+        If m0 = m4 Then
+            txt_01.Text = "第一題結果：" & m0 & " is a palindrome."
+        Else
+            txt_01.Text = "第一題結果：" & m0 & " is not a palindrome."
+        End If
+    End Sub
+
+    Private Sub btn_print_Click(sender As Object, e As EventArgs) Handles btn_print.Click
+        Call pd.Print()
+    End Sub
+
+    Private Sub pd_PrintPage(sender As Object, e As Printing.PrintPageEventArgs) Handles pd.PrintPage
+        Dim m0 As String = ""
+
+        m0 = m0 & "姓名：[陳紋誼]　　　　術科測試編號：[000001]" & vbCrLf    '換行符號：vbCrLf
+        m0 = m0 & "座號：[01]　　　　　　日期：[2023/08/03]" & vbCrLf & vbCrLf  '標點符號和空格要全形
+        m0 = m0 & txt_01.Text
+
+        e.Graphics.DrawString(m0, txt_01.Font, Brushes.Black, 100, 100)
+        '列印：Graphics.DrawString(內容, 字型, 字的顏色, 列印位置, 列印位置)
+    End Sub
+End Class
