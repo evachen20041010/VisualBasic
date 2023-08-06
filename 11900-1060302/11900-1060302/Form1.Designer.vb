cf56f60942544a7a7e7d3b7d25ff23df0556fec7
EvaChen <82190631+evachen20041010@users.noreply.github.com>
2023-08-06 23:29:05 +0800

    Add files via upload

---
+++ 11900-1060302/11900-1060302/Form1.Designer.vb
@@ -0,0 +1,79 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 覆寫 Dispose 以清除元件清單。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '為 Windows Form 設計工具的必要項
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下為 Windows Form 設計工具所需的程序
+    '可以使用 Windows Form 設計工具進行修改。
+    '請勿使用程式碼編輯器進行修改。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btn_02 = New System.Windows.Forms.Button()
+        Me.btn_print = New System.Windows.Forms.Button()
+        Me.txt_01 = New System.Windows.Forms.TextBox()
+        Me.pd = New System.Drawing.Printing.PrintDocument()
+        Me.SuspendLayout()
+        '
+        'btn_02
+        '
+        Me.btn_02.Location = New System.Drawing.Point(200, 45)
+        Me.btn_02.Name = "btn_02"
+        Me.btn_02.Size = New System.Drawing.Size(122, 47)
+        Me.btn_02.TabIndex = 0
+        Me.btn_02.Text = "第二題"
+        Me.btn_02.UseVisualStyleBackColor = True
+        '
+        'btn_print
+        '
+        Me.btn_print.Location = New System.Drawing.Point(467, 45)
+        Me.btn_print.Name = "btn_print"
+        Me.btn_print.Size = New System.Drawing.Size(109, 47)
+        Me.btn_print.TabIndex = 1
+        Me.btn_print.Text = "列印答案"
+        Me.btn_print.UseVisualStyleBackColor = True
+        '
+        'txt_01
+        '
+        Me.txt_01.Location = New System.Drawing.Point(108, 139)
+        Me.txt_01.Multiline = True
+        Me.txt_01.Name = "txt_01"
+        Me.txt_01.Size = New System.Drawing.Size(577, 249)
+        Me.txt_01.TabIndex = 2
+        '
+        'pd
+        '
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.txt_01)
+        Me.Controls.Add(Me.btn_print)
+        Me.Controls.Add(Me.btn_02)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btn_02 As Button
+    Friend WithEvents btn_print As Button
+    Friend WithEvents txt_01 As TextBox
+    Friend WithEvents pd As Printing.PrintDocument
+End Class
---
+++ 11900-1060302/11900-1060302/Form1.vb
@@ -0,0 +1,62 @@
+﻿Public Class Form1
+    '******************************
+    '* 11900-1060302 Program Start *
+    '******************************
+    Private Sub btn_02_Click(sender As Object, e As EventArgs) Handles btn_02.Click
+        Dim m0 As Integer   '存三角形高度
+
+        FileOpen(1, "C:\Users\mshome\source\11900\範例光碟\1060302.SM", OpenMode.Input) '開啟檔案(模式：讀取)
+        Input(1, m0)    '存檔案的值
+        FileClose(1)    '關檔
+
+        Dim m1 As String = ""   '存列印結果
+        Dim i, j As Integer
+
+        '(For...Next)
+        'For i = 1 To m0
+        '    For j = 1 To i
+        '        m1 = m1 & j
+        '    Next
+        '    m1 = m1 & vbCrLf
+        'Next
+
+        '(Do While...Loop)
+        'i = 1
+        'Do While i <= m0
+        '    j = 1
+        '    Do While j <= i
+        '        m1 = m1 & j
+        '        j = j + 1
+        '    Loop
+        '    m1 = m1 & vbCrLf
+        '    i = i + 1
+        'Loop
+
+        '(Do...Loop While)
+        i = 1
+        Do
+            j = 1
+            Do
+                m1 = m1 & j
+                j = j + 1
+            Loop While j <= i
+            m1 = m1 & vbCrLf
+            i = i + 1
+        Loop While i <= m0
+
+        txt_01.Text = "第二題結果：" & vbCrLf & m1
+    End Sub
+
+    Private Sub btn_print_Click(sender As Object, e As EventArgs) Handles btn_print.Click
+        Call pd.Print()
+    End Sub
+
+    Private Sub pd_PrintPage(sender As Object, e As Printing.PrintPageEventArgs) Handles pd.PrintPage
+        Dim m1 As String = ""
+
+        m1 = m1 & "姓名：[陳紋誼]　　　　術科測試編號：[000001]" & vbCrLf
+        m1 = m1 & "座號：[01]　　　　　　日期：[2023/08/06]" & vbCrLf & vbCrLf
+        m1 = m1 & txt_01.Text
+        e.Graphics.DrawString(m1, txt_01.Font, Brushes.Black, 100, 100)
+    End Sub
+End Class
