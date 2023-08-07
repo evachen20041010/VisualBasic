73169ebbadecb6ee0bfb6b7be08223776715a2bb
EvaChen <82190631+evachen20041010@users.noreply.github.com>
2023-08-07 21:41:09 +0800

    Add files via upload

---
+++ 11900-1060303/11900-1060303/Form1.Designer.vb
@@ -0,0 +1,79 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 覆寫 Dispose 以清除元件清單。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '為 Windows Form 設計工具的必要項
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下為 Windows Form 設計工具所需的程序
+    '可以使用 Windows Form 設計工具進行修改。
+    '請勿使用程式碼編輯器進行修改。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btn_03 = New System.Windows.Forms.Button()
+        Me.btn_print = New System.Windows.Forms.Button()
+        Me.txt_01 = New System.Windows.Forms.TextBox()
+        Me.pd = New System.Drawing.Printing.PrintDocument()
+        Me.SuspendLayout()
+        '
+        'btn_03
+        '
+        Me.btn_03.Location = New System.Drawing.Point(213, 46)
+        Me.btn_03.Name = "btn_03"
+        Me.btn_03.Size = New System.Drawing.Size(110, 44)
+        Me.btn_03.TabIndex = 0
+        Me.btn_03.Text = "第三題"
+        Me.btn_03.UseVisualStyleBackColor = True
+        '
+        'btn_print
+        '
+        Me.btn_print.Location = New System.Drawing.Point(435, 46)
+        Me.btn_print.Name = "btn_print"
+        Me.btn_print.Size = New System.Drawing.Size(107, 44)
+        Me.btn_print.TabIndex = 1
+        Me.btn_print.Text = "列印答案"
+        Me.btn_print.UseVisualStyleBackColor = True
+        '
+        'txt_01
+        '
+        Me.txt_01.Location = New System.Drawing.Point(92, 119)
+        Me.txt_01.Multiline = True
+        Me.txt_01.Name = "txt_01"
+        Me.txt_01.Size = New System.Drawing.Size(578, 282)
+        Me.txt_01.TabIndex = 2
+        '
+        'pd
+        '
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.txt_01)
+        Me.Controls.Add(Me.btn_print)
+        Me.Controls.Add(Me.btn_03)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btn_03 As Button
+    Friend WithEvents btn_print As Button
+    Friend WithEvents txt_01 As TextBox
+    Friend WithEvents pd As Printing.PrintDocument
+End Class
---
+++ 11900-1060303/11900-1060303/Form1.vb
@@ -0,0 +1,60 @@
+﻿Public Class Form1
+    '******************************
+    '* 11900-1060303 Program Start *
+    '******************************
+    Private Sub btn_03_Click(sender As Object, e As EventArgs) Handles btn_03.Click
+        Dim m0 As Integer = 0   '存輸入的數字
+
+        FileOpen(1, "C:\Users\mshome\source\11900\範例光碟\1060303.SM", OpenMode.Input) '開啟檔案(模式：讀取)
+        Input(1, m0)    '存檔案的值
+        FileClose(1)    '關檔
+
+        Dim i As Integer = 0
+        Dim x As Integer = 0   '可以整除的數有多少
+
+        '(For...Next)
+        'For i = 1 To m0
+        '    If m0 Mod i = 0 Then
+        '        x = x + 1
+        '    End If
+        'Next
+
+        '(Do While...Loop)
+        'i = 1
+        'Do While i <= m0
+        '    If m0 Mod i = 0 Then
+        '        x = x + 1
+        '    End If
+        '    i = i + 1
+        'Loop
+
+        '(Do...Loop While)
+        i = 1
+        Do
+            If m0 Mod i = 0 Then
+                x = x + 1
+            End If
+            i = i + 1
+        Loop While i <= m0
+
+        If x = 2 Then
+            txt_01.Text = "第三題結果：" & m0 & " is a prime number."
+        Else
+            txt_01.Text = "第三題結果：" & m0 & " is not a prime number."
+        End If
+    End Sub
+
+    Private Sub btn_print_Click(sender As Object, e As EventArgs) Handles btn_print.Click
+        Call pd.Print()
+    End Sub
+
+    Private Sub pd_PrintPage(sender As Object, e As Printing.PrintPageEventArgs) Handles pd.PrintPage
+        Dim m0 As String = ""
+
+        m0 = m0 & "姓名：[陳紋誼]　　　　術科測試編號：[000001]" & vbCrLf
+        m0 = m0 & "座號：[01]　　　　　　日期：[2023/08/07]" & vbCrLf & vbCrLf
+        m0 = m0 & txt_01.Text & vbCrLf
+
+        e.Graphics.DrawString(m0, txt_01.Font, Brushes.Black, 100, 100)
+    End Sub
+End Class
