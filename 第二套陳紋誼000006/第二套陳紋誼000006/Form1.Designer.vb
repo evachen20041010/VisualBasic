cba74904f8c72cc7b1a57d3de5ff151b4feb3a7a
EvaChen <82190631+evachen20041010@users.noreply.github.com>
2023-08-25 18:45:32 +0800

    Add files via upload

---
+++ 第二套陳紋誼000006/第二套陳紋誼000006/Form1.Designer.vb
@@ -0,0 +1,160 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 覆寫 Dispose 以清除元件清單。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '為 Windows Form 設計工具的必要項
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下為 Windows Form 設計工具所需的程序
+    '可以使用 Windows Form 設計工具進行修改。
+    '請勿使用程式碼編輯器進行修改。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.dgv = New System.Windows.Forms.DataGridView()
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.dgv, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.TextBox4)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.TextBox3)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.TextBox2)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.TextBox1)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(776, 131)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "應檢人資料"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(35, 43)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(37, 15)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "姓名"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(78, 33)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(158, 25)
+        Me.TextBox1.TabIndex = 1
+        Me.TextBox1.Text = "陳紋誼"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(78, 86)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(158, 25)
+        Me.TextBox2.TabIndex = 3
+        Me.TextBox2.Text = "01"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(35, 96)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(37, 15)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "座號"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(529, 33)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(158, 25)
+        Me.TextBox3.TabIndex = 5
+        Me.TextBox3.Text = "000006"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(426, 43)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(97, 15)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "術科測試編號"
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(529, 86)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(158, 25)
+        Me.TextBox4.TabIndex = 7
+        Me.TextBox4.Text = "2023/08/25"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(424, 96)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(99, 15)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "考  試   日   期"
+        '
+        'dgv
+        '
+        Me.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgv.Location = New System.Drawing.Point(12, 149)
+        Me.dgv.Name = "dgv"
+        Me.dgv.RowHeadersWidth = 51
+        Me.dgv.RowTemplate.Height = 27
+        Me.dgv.Size = New System.Drawing.Size(776, 477)
+        Me.dgv.TabIndex = 1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 638)
+        Me.Controls.Add(Me.dgv)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "身分證號碼檢查"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        CType(Me.dgv, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents dgv As DataGridView
+End Class
---
+++ 第二套陳紋誼000006/第二套陳紋誼000006/Form1.vb
@@ -0,0 +1,83 @@
+﻿Public Class Form1
+    '******************************
+    '* 11900-1060306 Program Start *
+    '******************************
+
+    Dim rno As Integer = 0
+    Dim data(100, 4) As String
+    Dim ec As String = ""
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Call rdata()
+        For i = 1 To rno
+            ec = ""
+            If ec = "" Then sp1(i)
+            If ec = "" Then sp2(i)
+            If ec = "" Then sp3(i)
+            data(i, 4) = ec
+        Next
+        Call wdata()
+    End Sub
+
+    Sub rdata()
+        FileOpen(1, "C:\Users\mshome\source\11900\範例光碟\1060306.SM", OpenMode.Input)
+        Do While Not EOF(1)
+            rno = rno + 1
+            Input(1, data(rno, 1))
+            Input(1, data(rno, 2))
+            Input(1, data(rno, 3))
+        Loop
+        FileClose(1)
+    End Sub
+
+    Sub sp1(ByVal i)
+        If Not data(i, 1) Like "[A-Z]#########" Then
+            ec = "FORMAT ERROR"
+        End If
+    End Sub
+
+    Sub sp2(ByVal i)
+        Dim d As String
+        d = Mid(data(i, 1), 2, 1) & data(i, 3)
+        If d <> "1M" And d <> "2F" Then
+            ec = "SEX CODE ERROR"
+        End If
+    End Sub
+
+    Sub sp3(ByVal i)
+        Dim l1 As String
+        Dim pid_e As String = "ABCDEFGHJKLMNPQRSTUVXYWZIO"
+        Dim x, x1, x2, y As Integer
+        l1 = Mid(data(i, 1), 1, 1)
+        x = InStr(pid_e, l1) + 9
+        x1 = x \ 10
+        x2 = x Mod 10
+        y = x1 + x2 * 9
+        For j = 2 To 9
+            y = y + Mid(data(i, 1), j, 1) * (10 - j)
+        Next
+        y = y + Mid(data(i, 1), 10, 1)
+        If y Mod 10 <> 0 Then
+            ec = "CHECK SUM ERROR"
+        End If
+    End Sub
+
+    Sub wdata()
+        Dim dt As New DataTable
+        dt.Columns.Add("ID_NO")
+        dt.Columns.Add("NAME")
+        dt.Columns.Add("SEX")
+        dt.Columns.Add("ERROR")
+
+        For i = 1 To rno
+            Dim dr As DataRow = dt.NewRow
+            dr(0) = data(i, 1)
+            dr(1) = data(i, 2)
+            dr(2) = data(i, 3)
+            dr(3) = data(i, 4)
+            dt.Rows.Add(dr)
+        Next
+
+        dgv.DataSource = dt
+        dgv.Sort(dgv.Columns(0), 0)
+    End Sub
+End Class
